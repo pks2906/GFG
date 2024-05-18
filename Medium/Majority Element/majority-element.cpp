975991f5033be17f23daab2f6fbe21d7a922a179
Pratik Sinha <91456061+pks2906@users.noreply.github.com>
2024-05-18 19:40:47 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Majority Element/majority-element.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    int majorityElement(int a[], int size)
+    {
+        
+        int maj_index = 0, count = 1;
+        for (int i = 1; i < size; i++) {
+            if (a[maj_index] == a[i])
+                count++;
+            else
+                count--;
+            if (count == 0) {
+                maj_index = i;
+                count = 1;
+            }
+        }
+
+       
+        count = 0;
+        for (int i = 0; i < size; i++)
+            if (a[i] == a[maj_index])
+                count++;
+        if (count > size/2)
+            return a[maj_index];
+        else
+            return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+        int arr[n];
+        
+        for(int i = 0;i<n;i++){
+            cin >> arr[i];
+        }
+        Solution obj;
+        cout << obj.majorityElement(arr, n) << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
